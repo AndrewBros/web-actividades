3727a927e1f2580282e663996f17ea855d4aa994
Andrés <67500897+AndrewBros@users.noreply.github.com>
2024-05-24 12:53:56 +0200

    sistema de donación

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express")
 const app = express()
 const port = 8081
 
+app.set("views", "views")
+app.set("views engine", "ejs")
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}))
+
 let routerActivities = require("./routers/routerActivities")
 app.use("/activities", routerActivities)
 
--- routers/routerActivities.js
+++ routers/routerActivities.js
@@ -1,10 +1,37 @@
 const express = require("express")
 const routerActivities = express.Router();
-let activities = require("../data/activites")
+
+let activities = require("../data/activities")
+let users = require("../data/users")
 
 routerActivities.get("/", (req,res) => {
     res.render("index.ejs", {
         acts: activities
     })
+})
+routerActivities.post("/donate", (req, res) =>{
+    let userName = req.body.userName
+    let password = req.body.password
+    let activityName = req.body.activityName
+    let money = parseFloat(req.body.money)
+
+    // Check data
+    let user = users.find(u => u.name == userName && u.password == password)
+    if (user == null){
+        res.send("Error")
+        return
+    }
+    if (user.money <= money){
+        res.send("Not enough money")
+        return
+    }
+    let activity = activities.find(a => a.name == activityName)
+    if(activity == null){
+        res.send("Not valid name")
+        return
+    }
+    user.money = user.money - money
+    activity.money = activity.money + money
+    res.send("/activities")
 })
 module.exports = routerActivities
